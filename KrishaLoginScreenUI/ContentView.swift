3d18ecb8313aea09bf1e3c704a69a5f10997d6f3
K-risha188 <krisharathod1645@gmail.com>
2025-01-24 14:55:11 +0530

    Initial Commit

---
+++ KrishaLoginScreenUI/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  KrishaLoginScreenUI
+//
+//  Created by student on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.pink.opacity(0.9)
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.25))
+                Circle()
+                    .scale(1.3)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("Login")
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height:50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red,width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height:50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red,width: CGFloat(wrongPassword))
+                    
+                    Button("Login"){
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width:300, height:50)
+                    .background(Color.pink)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+                
+            }
+            .navigationBarHidden(true)
+        }
+    }
+    func authenticateUser(username: String, password: String){
+        if username.lowercased() == "krisha123"{
+            wrongUsername = 0
+            if password.lowercased() == "123456"{
+                wrongPassword = 0
+                showingLoginScreen == true
+            }else{
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KrishaLoginScreenUI/KrishaLoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrishaLoginScreenUIApp.swift
+//  KrishaLoginScreenUI
+//
+//  Created by student on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct KrishaLoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
